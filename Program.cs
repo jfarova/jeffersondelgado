8288bc92b65e5060e33545332b978db6091f652d
chenaoh <jfarova112333@gmail.com>
2022-11-20 19:44:07 -0500

    Escribir un programa que escriba todos los múltiplos de 3 del número 1 al 3000

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         int f,n=3,d;
+            for(f=1;f<=3000;f++)
+            
+            {
+                
+                d=f*n;
+                Console.WriteLine(d);
+              
+
+
+            }
+            Console.ReadKey();
+    }
+}
